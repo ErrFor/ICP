ee3da79e0610cc8f6a4009661b76a411ac7b388d
chipsy <tima.kininbaev@gmail.com>
2024-05-01 06:22:55 +0200

    obstacle detection implemented

--- robots.cpp
+++ robots.cpp
@@ -3,6 +3,7 @@
 #include <QPainter>
 #include "qgraphicsscene.h"
 #include "robots.h"
+#include "obstacle.h"
 
 void AutonomousRobot::move() {
     double radAngle = orientation * M_PI / 180;
@@ -17,26 +18,39 @@ void AutonomousRobot::move() {
 }
 
 bool AutonomousRobot::detectObstacle() {
+    qDebug() << "Checking for obstacles for robot at position" << positionX << positionY;
+
     double radAngle = orientation * M_PI / 180;
     double dx = detectionRadius * cos(radAngle);
     double dy = detectionRadius * sin(radAngle);
 
-    // Check for scene boundaries
+    QRectF robotFuturePosition(positionX + dx, positionY + dy, 20, 20);
     QGraphicsScene* currentScene = scene();
     QRectF sceneRect = currentScene->sceneRect();
-    QRectF robotFuturePosition(positionX + dx, positionY + dy, 20, 20); // Assuming robot size is 20x20
 
     if (!sceneRect.contains(robotFuturePosition)) {
-        return true; // Robot is about to move out of the scene bounds
+        qDebug() << "Obstacle detected: out of scene bounds";
+        return true;
     }
 
-    // Existing obstacle detection logic remains the same
-    QList<QGraphicsItem*> itemsFound = currentScene->items(mapToScene(robotFuturePosition).boundingRect(), Qt::IntersectsItemShape);
-    for (QGraphicsItem* item : itemsFound) {
-        if (item != this && item->collidesWithItem(this)) {
+    // Рассчитаем правильную область обнаружения
+    QPainterPath detectionPath;
+    QPointF frontCenter = QPointF(positionX + cos(radAngle) * 20, positionY + sin(radAngle) * 20); // 20 - это предполагаемый "радиус" робота
+    detectionPath.moveTo(frontCenter);
+    detectionPath.addRect(QRectF(frontCenter.x(), frontCenter.y(), detectionRadius, 20)); // Эта область теперь корректно ориентирована
+
+    detectionPath.addRect(QRectF(positionX, positionY, detectionRadius, 20));
+    QList<QGraphicsItem*> collidingItems = scene()->items(detectionPath);
+
+    foreach(QGraphicsItem *item, collidingItems) {
+        if (item != this && dynamic_cast<Obstacle*>(item)) {
+            qDebug() << "Obstacle detected: collision with an obstacle";
             return true;
         }
     }
+
+    qDebug() << "No obstacles detected";
     return false;
 }
+
 
